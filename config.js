6c042d8a51fa508ce28b9b6edf9de01805400167
Youngtips <98290454+Youngtips@users.noreply.github.com>
2022-09-06 10:32:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -42,23 +42,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['918270026275','916380260672'] //you can add multiple number
-global.ownername = "𝗧𝚯𝗫𝗜𝗖 𝗧𝗨𝗥𝗕𝚯"
+global.owner = ['263787778679','263787778679'] //you can add multiple number
+global.ownername = "YOUNGTIPS"
 global.ytname = "YT: Turbo Mods" //ur yt chanel name
 global.socialm = "GitHub: TURBOHYPER" //ur github or insta name
 global.location = "India, TamilNadu, Coimbatore" //ur location
 
 //bot bombdy
-global.botname = "𝗧𝚯𝗫𝗜𝗖 ₪ 𝚫𝗟𝗘𝗫𝚫" //ur bot name
+global.botname = "VIPER 360" //ur bot name
 global.websitex = "https://github.com/TURBOHYPER" //ur website
 global.vidmenu = fs.readFileSync("./TurboMedia/theme/alexa.mp4") //gif and video menu
 global.packname = "𝗧𝚯𝗫𝗜𝗖 ₪ 𝚫𝗟𝗘𝗫𝚫"
 global.author = "𝗧𝚯𝗫𝗜𝗖 𝗧𝗨𝗥𝗕𝚯"
 global.themeemoji = "💖"
 global.reactmoji = "🌹"
-global.ownertag = ['916380260672'] //ur owner number
-global.ownernummenu = ['916380260672'] //ur owner number in menu and all
-global.watermark = " 𝗧𝚯𝗫𝗜𝗖 𝗧𝗨𝗥𝗕𝚯" //ur watermark
+global.ownertag = ['263787778679'] //ur owner number
+global.ownernummenu = ['263787778679'] //ur owner number in menu and all
+global.watermark = " 𝗧𝚯𝗫𝗜𝗖 " //ur watermark
 global.botscript = "https://github.com/TURBOHYPER/Toxic-Alexa_V3"
 global.linkz1 = "https://chat.whatsapp.com/LWjJ4tu2qe9BWQZ1JzRZgp"
 global.linkz2 = "https://chat.whatsapp.com/GsgzyBU8Vld0YCNnFrBAQt"
@@ -93,7 +93,7 @@ global.ntilink = []
 global.lolhuman = "KaysaS"
 global.sessionName = "session"
 global.antitags = false
-global.prefa = ['','!','.','🐦','🐤','😽']
+global.prefa = [''#']
 global.mess = {
     success: 'Done✓',
     admin: 'Tʜɪs Fᴇᴀᴛᴜʀᴇ Is Oɴʟʏ Fᴏʀ Aᴅᴍɪɴ',
